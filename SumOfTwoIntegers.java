9ecc4dfc9e06c25c4fff6f3a201236d095812718
jacksonfernando <jacksonfernando51@gmail.com>
2025-12-27 20:53:57 +0700

    add SumOfTwoIntegers

---
+++ SumOfTwoIntegers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Solution {
+  public static int sumOfTwoIntegers(int a, int b) {
+    while (b != 0) {
+      int carry = (a & b) << 1;
+      a = a ^ b;
+      b = carry;
+    }
+    return a;
+  }
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int a = Integer.parseInt(scanner.nextLine());
+    int b = Integer.parseInt(scanner.nextLine());
+    scanner.close();
+    int res = sumOfTwoIntegers(a, b);
+    System.out.println(res);
+  }
+}
